ec6e60590ca070d3c05fd47aabb8b0e167ea9272
manojkr1238 <manojkr1238@gmail.com>
2016-03-23 07:52:18 +0530

    .

---
+++ TCServices/CustomerTypeService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TCDBEntities;
+using TCRepositories;
+
+namespace TCServices
+{
+    class CustomerTypeService : ICustomerTypeService
+    { 
+        GenericRepository<CustomerType> _genericRepository;
+
+        public CustomerTypeService(DbContext dbContext)
+        {
+            this._genericRepository = new GenericRepository<CustomerType>(dbContext);
+        }
+        public IQueryable<CustomerType> GetAllCustomerTypes()
+        {
+            return _genericRepository.GetAll();
+        }
+        public CustomerType GetCustomerTypeById(int id)
+        {
+            return _genericRepository.GetById(id);
+        }
+        public int DeleteCustomerType(int id)
+        {
+            return _genericRepository.Delete(id);
+        }
+        public int UpdateCustomerType(CustomerType customerType)
+        {
+            return _genericRepository.Update(customerType);
+        }
+        public int InsertCustomerType(CustomerType customerType)
+        {
+            return _genericRepository.Insert(customerType);
+        }
+    }
+}
