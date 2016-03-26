5af722506a86f9a4db71cf4665ac9c9f40680200
manojkr1238 <manojkr1238@gmail.com>
2016-03-26 13:14:12 +0530

    .

--- TCDBEntities/TownCleanEntityModel.cs
+++ TCDBEntities/TownCleanEntityModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace TCDBEntities
 {
@@ -18,5 +20,39 @@ namespace TCDBEntities
         public decimal? TotalVolume { get; set; }
         public decimal? TotalPrice { get; set; }
         public bool IsSent { get; set; }
+    }
+
+    public class InsertQuotation
+    {
+        [Required]
+        public string ContactName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        public string Email { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        [Required]
+        public string CustomerTypeID { get; set; }
+        [Required]
+        public string QuotationServiceTypeID { get; set; }
+
+        public bool IsExitingCustomer { get; set; }
+
+        public int? CustomerID { get; set; }
+
+        //[Required]
+        //public int BranchID { get; set; }
+
+        //[Required]
+        //public string CreatedBy { get; set; }
+
+        public SelectList CustomerTypeList { get; set; }
+        public SelectList QuotationServiceTypeList { get; set; }
+        public SelectList CustomerList { get; set; }
+
     }
 }
