39807abc384fb92fa9ba3f14179a2fb36b2a736a
manojkr1238 <manojkr1238@gmail.com>
2016-03-30 16:56:00 +0530

    30/03/2016

--- TownCleanWeb/Areas/Admin/Controllers/ExpenseController.cs
+++ TownCleanWeb/Areas/Admin/Controllers/ExpenseController.cs
@@ -9,12 +9,12 @@ using TownCleanWeb.Controllers;
 
 namespace TownCleanWeb.Areas.Admin.Controllers
 {
-   
+
     public class ExpenseController : BaseController
     {
 
         private TownClean_DBEntities db = new TownClean_DBEntities();
-        
+
         private IExpenseService _ExpenseService;
 
         public ExpenseController(IExpenseService ExpenseService)
@@ -22,7 +22,7 @@ namespace TownCleanWeb.Areas.Admin.Controllers
             this._ExpenseService = ExpenseService;
         }
         // GET: Admin/Expense
-       
+
 
         public ActionResult ExpenseDetails(int id)
         {
@@ -31,35 +31,35 @@ namespace TownCleanWeb.Areas.Admin.Controllers
         }
         public ActionResult AddNewExpense()
         {
-           
+
             return View();
         }
-     /*   [HttpPost]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult AddNewExpense([Bind(Include = "ExpenseId,ExpenseName,Amount,ExpenseType,DISCRIPTION")] Expense expense)
+        public ActionResult Create([Bind(Include = "ExpenseID,ExpenseName,ExpenseType,Amount,ExpenseDate,Description,PaymentMode,PaymentModeNo,Attachment_Url,BranchID,CreatedBy,CreatedDate,ModifiedBy,ModifiedDate,IsDeleted")] Expense expense)
         {
             if (ModelState.IsValid)
             {
                 db.Expenses.Add(expense);
                 db.SaveChanges();
-               
+                
             }
 
+            ViewBag.BranchID = new SelectList(db.Branches, "BranchID", "BranchName", expense.BranchID);
             return View(expense);
-        }*/
+        }
 
-    
-      
-      
+
+
         public ActionResult ExpenseList()
         {
             var expenseList = _ExpenseService.GetExpenseSummaryList().ToList();
             return View(expenseList);
         }
-       
 
-        [HttpPost]
-     public ActionResult AddNewExpense(InsertExpense model)
+
+       /* [HttpPost]
+        public ActionResult AddNewExpense(InsertExpense model)
         {
             int i = 0;
             if (!ModelState.IsValid)
@@ -77,25 +77,25 @@ namespace TownCleanWeb.Areas.Admin.Controllers
             qc.Attachment_Url = model.Attachment_Url;
             qc.BranchID = branchID;
             qc.CreatedBy = userName;
-           
+
             if (model.IsExitingExpense)
                 qc.ExpenseName = model.ExpenseName;
             else
                 qc.ExpenseName = null;
 
             i = _ExpenseService.InsertExpense(qc);
-            
-           
+
+
             if (i > 0)
                 return RedirectToAction("AddItemsForExpense", new { id = qc.ExpenseID });
             else
             {
                 return View(model);
             }
-        }
+        }*/
     }
 
 
-      
-     
-    }
+
+
+}
---
+++ TownCleanWeb/Areas/Admin/Controllers/ExpensesController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TCDBEntities;
+using TCServices;
+using TownCleanWeb.Controllers;
+
+namespace TownCleanWeb.Areas.Admin.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private TownClean_DBEntities db = new TownClean_DBEntities();
+        
+
+       
+        
+       
+        // GET: Admin/Expenses
+        public ActionResult Index()
+        {
+            var expenses = db.Expenses.Include(e => e.Branch);
+            return View(expenses.ToList());
+        }
+        
+        // GET: Admin/Expenses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expense expense = db.Expenses.Find(id);
+            if (expense == null)
+            {
+                return HttpNotFound();
+            }
+            return View(expense);
+        }
+
+        // GET: Admin/Expenses/Create
+        public ActionResult Create()
+        {
+            ViewBag.BranchID = new SelectList(db.Branches, "BranchID", "BranchName");
+            return View();
+        }
+
+        // POST: Admin/Expenses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ExpenseID,ExpenseName,ExpenseType,Amount,ExpenseDate,Description,PaymentMode,PaymentModeNo,Attachment_Url,BranchID,CreatedBy,CreatedDate,ModifiedBy,ModifiedDate,IsDeleted")] Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Expenses.Add(expense);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.BranchID = new SelectList(db.Branches, "BranchID", "BranchName", expense.BranchID);
+            return View(expense);
+        }
+
+        // GET: Admin/Expenses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expense expense = db.Expenses.Find(id);
+            if (expense == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.BranchID = new SelectList(db.Branches, "BranchID", "BranchName", expense.BranchID);
+            return View(expense);
+        }
+
+        // POST: Admin/Expenses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ExpenseID,ExpenseName,ExpenseType,Amount,ExpenseDate,Description,PaymentMode,PaymentModeNo,Attachment_Url,BranchID,CreatedBy,CreatedDate,ModifiedBy,ModifiedDate,IsDeleted")] Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(expense).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.BranchID = new SelectList(db.Branches, "BranchID", "BranchName", expense.BranchID);
+            return View(expense);
+        }
+
+        // GET: Admin/Expenses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Expense expense = db.Expenses.Find(id);
+            if (expense == null)
+            {
+                return HttpNotFound();
+            }
+            return View(expense);
+        }
+
+        // POST: Admin/Expenses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Expense expense = db.Expenses.Find(id);
+            db.Expenses.Remove(expense);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
